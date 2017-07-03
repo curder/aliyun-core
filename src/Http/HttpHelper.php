dbe6d696fc94e6291b3e31de0b95b3670f513d0d
curd <curder@foxmail.com>
2017-07-03 18:48:11 +0800

    typo fix

--- src/Http/HttpHelper.php
+++ src/Http/HttpHelper.php
@@ -1,4 +1,5 @@
 <?php namespace Curder\AliyunCore\Http;
+use Curder\AliyunCore\Exception\ClientException;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
